69f27af72cb6d811c8114011e57a22f34127201b
Michael Kardjian <makardjian@gmail.com>
2018-12-11 14:22:21 -0800

    got my server to serve up my html file by using express.static

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+
+
+
+
+const app = express();
+const PORT = 3000;
+app.listen(PORT);
+
+
+
+
+app.use(express.static(path.join(__dirname, './client')));
+//THE ABOVE LINE MEANS I DON'T EVEN NEED TO USE THE BELOW GET METHOD TO SERVE UP MY INDEX.HTML
+// app.get('/reportGen', (req, res) => {
+// 	res.sendFile(path.join(__dirname, './client/'))
+// });
+
+
+
+app.post('/reportGen', (req, res) => {
+	res.send("hello, world")
+})
+
 
 
