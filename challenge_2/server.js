ce81f9fdf3265ab9fc92398a5b021c3f690adb09
Michael Kardjian <makardjian@gmail.com>
2018-12-12 00:15:47 -0800

    implemented recursive function to render csv to the dom

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
-
+// const Helper = require('./client/app.js')
 
 
 
@@ -19,10 +20,78 @@ app.use(express.static(path.join(__dirname, './client')));
 // });
 
 
+// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.post('/reportGen', (req, res) => {
-	res.send("hello, world")
+
+
+app.post('/', (req, res) => {
+	// console.log(req.body.message)
+	res.send(`<!DOCTYPE html>
+<html>
+<head>
+	<title>CSV Report Generator</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+	<div id="main">
+		<h1>CSV Report Generator</h1>
+		<form class="form-submit" action="/" method="post">
+			<textarea id='text-area' name="message" rows="10" cols="30"></textarea>
+			<button id="submit">Submit Form</button>
+		</form>
+	<div>${convert(req.body.message)}</div>
+	</div>
+</body>
+</html>`)
 })
+
+var convert = (json) => {
+	//itterate over the keys in the first object and for each key concatenate a string seperated by a space
+		//if the key is 'children', then set json = children[0]
+
+		var obj = JSON.parse(json);
+
+		var collumnNames = '';
+
+		for (var key in obj) {
+			if (key !== 'children') {
+			collumnNames += key + ' ';
+			} 
+		}
+
+	
+		var csv = `${collumnNames}\n`
+
+		var inner = (data) => {
+
+			for (var key in data) {
+				if (key !== 'children') {
+
+					csv+=data[key] + ' '
+
+				} else if (key === 'children') {
+					csv+='\n'
+					data[key].forEach(ele => {
+						inner(ele)
+					})
+				}
+			}
+			
+			csv+='\n' 
+			  
+		}
+
+
+		inner(obj)
+		return csv;
+
+}
+
+//as the data comes into the server, I can parse it and then reformat the data 
+
+
+
 
 
 
